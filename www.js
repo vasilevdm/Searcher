b4e23ae8ecbc128e8da46d48bbdfb653c41fa9d5
dmitr <vasilev.dm@gmail.com>
2018-10-19 09:25:32 +0300

    minor changes in config

--- www.js
+++ www.js
@@ -94,7 +94,11 @@ console.log(date_m);
 if(
 	(date_h==6 && date_m>=0 && date_m<=5)
 	||
+	(date_h==9 && date_m>=0 && date_m<=5)
+	||
 	(date_h==12 && date_m>=0 && date_m<=5)
+	||
+	(date_h==15 && date_m>=0 && date_m<=5)
 	||
 	(date_h==18 && date_m>=0 && date_m<=5)
 	) {
